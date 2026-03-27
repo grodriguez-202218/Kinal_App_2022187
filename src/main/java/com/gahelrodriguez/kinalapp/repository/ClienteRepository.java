ec1fa2bb89880f4c4819e5c620720488045fbebb
grodriguez-2022187 <grodriguez-2022187@kinal.edu.gt>
2026-03-27 15:01:50 -0600

    Revert "Grodriguez 2022187"

--- src/main/java/com/gahelrodriguez/kinalapp/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gahelrodriguez.kinalapp.repository;
-
-import com.gahelrodriguez.kinalapp.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
-    List<Cliente> findByEstado(int estado);
-}
